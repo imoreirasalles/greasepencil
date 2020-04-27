c24025f0bf0c444f3d56d6080e5eb92caa8502d6
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-04-27 17:20:46 -0300

    update script to template matching version

--- greasepencil.py
+++ greasepencil.py
@@ -1,83 +1,77 @@
 import os
-from skimage import exposure
-from skimage import io
-from skimage import img_as_ubyte
-from skimage.filters import threshold_otsu, threshold_local
-from skimage.measure import label, regionprops
-from skimage.morphology import closing, square, remove_small_objects
-from skimage.segmentation import clear_border
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from skimage.feature import match_template, peak_local_max
+from skimage.morphology import extrema
+from skimage.measure import label
+from skimage.segmentation import watershed
+from skimage import exposure
+from skimage import img_as_ubyte, io, exposure, color
 
 source = input("Em qual pasta estão as imagens matriz?")
 destination = input("Em qual pasta deseja salvar as imagens individualizadas?")
 
-# define cropper function
 def greasepencil(input_image):
 
     os.chdir(f"{source}")
 
-    # import and preprocess source image
-    original_image = img_as_ubyte(io.imread(input_image))
-    logarithmic_corrected = exposure.adjust_log(original_image, 2.1)
-    image_bw = logarithmic_corrected[:, :, 2]
+    # read image and determine template width
+    color_image = io.imread(input_image)
+    image = img_as_ubyte(io.imread(input_image, as_gray=True))
+    tmp_w = round(0.9*min(image.shape)/4)
 
-    # threshold and clean binary image
-    thresh = threshold_otsu(image_bw)
-    clean = closing(image_bw > thresh, square(75))
-    cleaner = remove_small_objects(clean, 10000)
+    # stablish relative measures
+    frame_left = round(tmp_w * 0.15)
+    frame_right = round(tmp_w * 0.85)
+    frame_top = round(tmp_w * 0.27)
+    frame_bottom = round(tmp_w * 0.73)
 
-    # label sub images
-    label_image = label(cleaner)
-    mask = label_image == 1
-    label_image[mask] = 0
-    region_list = []
-    for region in regionprops(label_image):
-        if 400000 > region.area >= 100000:
-            coords = region.bbox
-            region_list.append(coords)
-    print(f"A imagem {input_image} foi dividida em {len(region_list)} partes")
+    # build slide template
+    rel_template = np.full((tmp_w, tmp_w), 0.07)
+    rel_template[frame_top:frame_bottom, frame_left:frame_right] = 0.5
+    rel_template = np.pad(rel_template, (20,), constant_values=1)
+    rel_template += 0.1 * np.random.random(rel_template.shape)
 
+    v_template = np.rot90(rel_template)
 
-    '''create output directory
-    if not os.path.exists("../_output"):
-        os.mkdir("../_output")
-    os.chdir("..")'''
+    # template matching
+    h_result = match_template(color_image[:,:,0], rel_template, pad_input=True)
+    v_result = match_template(color_image[:,:,0], v_template, pad_input=True)
 
-    # order sub images from top-left to bottom-right
-    def origin_coord(item):
-        return (item[0] * 4 + item[1]) / 5
+    # find and sort matching peaks (top left to bottom right)
+    h_coordinates = peak_local_max(h_result, min_distance=int(tmp_w*0.8), threshold_abs=0.35, exclude_border=False)
+    coordinates_list = [[coor[0], coor[1]] for coor in h_coordinates]
+    coordinates_list.sort(key= lambda item: (item[0] * 4 + item[1]) / 5)
 
-    # get sub image coordinates
-    region_list.sort(key=origin_coord)
-    review_count = 0
-    for region in region_list:
-        counter = region_list.index(region) + 1
-        height = region[2] - region[0]
-        width = region[3] - region [1]
-        ratio = width/height
-        if ratio < 1:
-            ratio = 1/ratio
-
-        # save sub image and name it accordingly       
-        if 1.4 < ratio < 1.61:
-            io.imsave(
-                f"{destination}/{input_image[0:-4]}-{counter:02}.jpg",
-                original_image[region[0] : region[2], region[1] : region[3]], quality=100
-            )
-            
-        else:
-            io.imsave(
-                f"{destination}/{input_image[0:-4]}-{counter:02}-REVISAR.jpg",
-                original_image[region[0] : region[2], region[1] : region[3]], quality=100
-            )
-            review_count += 1 
-        
     
-    print(f"A imagem {input_image} gerou {len(review_count)} produtos problemáticos")
-        
+    # determine picture orientation and save file
+    counter = 1
 
-# list files to be processed
+    for point in (coordinates_list):
+        r, c = point
+        name, ext = str.split(input_image, ".")
+        if v_result[r, c] > h_result[r, c]:
+            io.imsave(
+                f"{destination}/{name}-{counter:02}.jpg", 
+                color_image
+                [r - round(tmp_w * 0.35) : r + round(tmp_w * 0.35), 
+                c - round(tmp_w * 0.23) : c + round(tmp_w * 0.23)], 
+                quality=100
+            )
+        else:       
+            io.imsave(
+                f"{destination}/{name}-{counter:02}.jpg",
+                color_image
+                [r - round(tmp_w * 0.23) : r + round(tmp_w * 0.23), 
+                c - round(tmp_w * 0.35) : c + round(tmp_w * 0.35)], 
+                quality=100
+            )
+        
+        counter += 1
+
+
+# create list of files to be processed
 extensions = (".jpg", ".jpeg")
 image_files = [
     image_file
@@ -88,4 +82,6 @@ image_files = [
 # call function
 for i in image_files:
     greasepencil(i)
+
+# final feedback
 print(f"Você forneceu {len(image_files)} imagens e gerou {len(os.listdir(destination))} arquivos!")
