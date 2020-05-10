140bf87fb31e1bf430d355a920ea67fd1691ad4e
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2020-05-10 11:38:23 -0300

    starting to organize the code

--- greasepencil.py
+++ greasepencil.py
@@ -8,6 +8,10 @@ from skimage.measure import label
 from skimage.segmentation import watershed
 from skimage import exposure, img_as_ubyte, io, color, transform
 
+"""
+PAD WITH BLACK TO CENTER TEMPLATE
+"""
+
 source = input("Em qual pasta estão as imagens matriz?")
 destination = input("Em qual pasta deseja salvar as imagens individualizadas?")
 previous_files = len(os.listdir(destination))
@@ -28,7 +32,7 @@ def trim_black_border(img,tol=0):
 
     return img[row_start:row_end,col_start:col_end]
 
-def greasepencil(input_image, ncol=4, dimension=None):
+def greasepencil(input_image, ncol=4, tmp_w=None):
 
     os.chdir(f"{source}")
 
@@ -36,17 +40,19 @@ def greasepencil(input_image, ncol=4, dimension=None):
     color_image = io.imread(input_image)
     image = img_as_ubyte(io.imread(input_image, as_gray=True))
     m, n = image.shape
-    if dimension is None:
+    if tmp_w is None:
         tmp_w = round(min(image.shape)/ncol)
-    else:
-        tmp_w = dimension
+        user_tmp_w = False
     # establish relative measures
+    
+
+def build_template(tmp_w):
+    # build slide template
     frame_left = round(tmp_w * 0.15)
     frame_right = round(tmp_w * 0.85)
     frame_top = round(tmp_w * 0.27)
     frame_bottom = round(tmp_w * 0.73)
-
-    # build slide template
+    
     rel_template = np.full((tmp_w, tmp_w), 0.07)
     rel_template[frame_top:frame_bottom, frame_left:frame_right] = 0.5
     rel_template = np.pad(rel_template, (30,), constant_values=1)
@@ -61,7 +67,7 @@ def greasepencil(input_image, ncol=4, dimension=None):
 
     for scale in np.linspace(0.5, 1.0, 6)[::-1]:
         
-        tmp_w = int(rel_template.shape[0]*scale)
+        tmp_w = tmp_w * scale
         resized = transform.resize(rel_template, (tmp_w, tmp_w))
 
         v_template = np.rot90(resized)
@@ -72,7 +78,7 @@ def greasepencil(input_image, ncol=4, dimension=None):
         h_coordinates = peak_local_max(h_result, min_distance=int(tmp_w*0.8), threshold_abs=0.35, exclude_border=False)
         coordinates_list = [[coor[0], coor[1]] for coor in h_coordinates]
 
-        if dimension:
+        if user_tmp_w:
             break
 
         peaks = [h_result[r, c] for r, c in h_coordinates]
@@ -99,13 +105,15 @@ def greasepencil(input_image, ncol=4, dimension=None):
     coordinates_list.sort(key= lambda item: (item[0] * 4 + item[1]) / 5)
 
     # determine picture orientation, trim and save file
-    counter = 1
 
-    for point in (coordinates_list):
+    detected_frames = []
+
+    for i, point in enumerate(coordinates_list):
         r, c = point
         name, ext = str.split(input_image, ".")
         large = round(tmp_w * 0.34)
         small = round(tmp_w * 0.22)
+        filename = f"{name}-{i+1:02}.jpg"
         if v_result[r, c] > h_result[r, c]:
             minr = np.clip(r - large, 0, None)
             maxr = np.clip(r + large, None, m)
@@ -117,33 +125,35 @@ def greasepencil(input_image, ncol=4, dimension=None):
             maxr = np.clip(r + small, None, m)
             minc = np.clip(c - large, 0, None)
             maxc = np.clip(c + large, None, n)
-        
-        picture = color_image[minr : maxr, minc : maxc]
-        picture = trim_black_border(picture, tol=100)
-
-        io.imsave(
-            f"{destination}/{name}-{counter:02}.jpg", picture, quality=100)
-        
-        counter += 1
 
 
-# create list of files to be processed
-extensions = (".jpg", ".jpeg")
-image_files = [
-    image_file
-    for image_file in os.listdir(f"{source}")
-    if str(image_file).endswith(tuple(extensions))
-]
+        detected_frames.append({i:i, filename:filename, minr:minr, maxr:maxr, minc:minc, maxc:maxc})
+    
+    return detected_frames
 
-# call function
-for i in image_files:
-    try:
-        greasepencil(i)
-    except Exception as e:
-        print(str(e))
-        
-# final feedback
-print(
-    f"Você forneceu {len(image_files)} imagens e gerou {len(os.listdir(destination))-previous_files} arquivos!")
+
+if __name__ == "__main__":
+
+    # create list of files to be processed
+    extensions = (".jpg", ".jpeg")
+    image_files = [
+        image_file
+        for image_file in os.listdir(f"{source}")
+        if str(image_file).endswith(tuple(extensions))
+    ]
+
+    # call function
+    for image in image_files:
+        try:
+            for frame in greasepencil(image):
+                picture = image[frame['minr']:frame['maxr'], frame['minc'], frame['maxc']]
+                picture = trim_black_border(picture, tol=100)
+                io.imsave(os.path.join(destination, picture["filename"]), picture, quality=100)                
+        except Exception as e:
+            print(str(e))
+
+    # final feedback
+    print(
+        f"Você forneceu {len(image_files)} imagens e gerou {len(os.listdir(destination))-previous_files} arquivos!")
 
 
